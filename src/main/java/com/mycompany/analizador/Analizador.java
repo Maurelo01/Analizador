10f2482634382ffef1f813d2eac27c10703d3913
maurelo860@gmail.com <maurelo860@gmail.com>
2025-09-29 19:39:36 -0600

    Main

---
+++ src/main/java/com/mycompany/analizador/Analizador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.analizador;
+
+/**
+ *
+ * @author mauricio
+ */
+public class Analizador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
